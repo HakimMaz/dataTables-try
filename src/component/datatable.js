f0b2e7a5a8e07ac8693089a560d332b1677693d1
HakimMaz <maz.hakim93@gmail.com>
2020-10-26 21:47:22 +0100

    add id filter

--- src/component/datatable.js
+++ src/component/datatable.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { DataGrid } from '@material-ui/data-grid';
+import {Button} from "@material-ui/core"
 export default class DataTable extends Component {
 
    constructor(props){
@@ -10,34 +11,29 @@ export default class DataTable extends Component {
    }
   
     render() {
+        const actions =<button style={{height:"10px",backgroundColor:"red"}}>Add</button>
+ 
+     
         const columns = [
             { field: 'id', headerName: 'ID', width: 70 },
-            { field: 'libelle', headerName: 'First name', width: 130 },
-            { field: 'description', headerName: 'Last name', width: 130 },
-            {
-              field: 'age',
-              headerName: 'Age',
-              type: 'number',
-              width: 90,
-            },
-            {
-              field: 'fullName',
-              headerName: 'Full name',
-              description: 'This column has a value getter and is not sortable.',
-              sortable: false,
-              width: 160,
-              valueGetter: (params) =>
-                `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-            },
+            { field: 'libelle', headerName: 'First name', width: 150 },
+            { field: 'description', headerName: 'Last name', width: 200 }, 
+            { field: <Button  variant="outlined" size="small" color="primary">
+            Edit
+        </Button>,
+                 
+                headerName: 'Action',
+                 width: 200 },   
           ];
           
           const listItem=this.props.data.filter(d=>
             d.libelle.includes(this.props.filter)||
-            d.description.includes(this.props.filter)
+            d.description.includes(this.props.filter)||
+            d.id.toString().includes(this.props.filter)
             )
         return (
             <div>
-            <div style={{ height: 400, width: '100%' }}>
+            <div style={{ height: 400, width: '70%' }}>
             <DataGrid rows={listItem} columns={columns} pageSize={5} checkboxSelection />
           </div>
             </div>
